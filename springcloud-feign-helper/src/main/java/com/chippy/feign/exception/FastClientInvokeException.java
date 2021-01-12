02a50cd262328f687ade68cc663d19e20baf900a
Chippyer <450799968@qq.com>
2021-01-12 20:07:51 +0800

    修复已知问题

--- springcloud-feign-helper/src/main/java/com/chippy/feign/exception/FastClientInvokeException.java
+++ springcloud-feign-helper/src/main/java/com/chippy/feign/exception/FastClientInvokeException.java
@@ -1,10 +1,13 @@
 package com.chippy.feign.exception;
 
+import lombok.Data;
+
 /**
  * FeignClientHelper使用时调度异常定义
  *
  * @author: chippy
  */
+@Data
 public class FastClientInvokeException extends RuntimeException {
 
     private int code = 200;
